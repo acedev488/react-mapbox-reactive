4ecb8e29e1e8159364654ef1e7b336412015c081
Austin Lee <austinl28312@gmail.com>
2023-05-23 21:51:10 -0500

    feat: add markers slice to the store (add/update/remove)

--- src/store/useMapStore.ts
+++ src/store/useMapStore.ts
@@ -1,9 +1,22 @@
 import { create } from 'zustand'
-import { DEFAULT_VIEWPORT, Viewport } from '../map/types'
+import { DEFAULT_VIEWPORT, LngLat, Viewport } from '../map/types'
+
+export interface MarkerData {
+  id: string
+  lngLat: LngLat
+  color: string
+  draggable: boolean
+  label?: string
+}
 
 interface MapStoreState {
   viewport: Viewport
   setViewport: (viewport: Viewport) => void
+
+  markers: MarkerData[]
+  addMarker: (marker: MarkerData) => void
+  updateMarker: (id: string, patch: Partial<Omit<MarkerData, 'id'>>) => void
+  removeMarker: (id: string) => void
 }
 
 export const useMapStore = create<MapStoreState>((set) => ({
@@ -11,4 +24,13 @@ export const useMapStore = create<MapStoreState>((set) => ({
   // Called both by the map itself (after the user pans/zooms) and by UI
   // controls that want to *drive* the map (e.g. a "reset view" button).
   setViewport: (viewport) => set({ viewport }),
+
+  markers: [],
+  addMarker: (marker) => set((s) => ({ markers: [...s.markers, marker] })),
+  updateMarker: (id, patch) =>
+    set((s) => ({
+      markers: s.markers.map((m) => (m.id === id ? { ...m, ...patch } : m)),
+    })),
+  removeMarker: (id) =>
+    set((s) => ({ markers: s.markers.filter((m) => m.id !== id) })),
 }))

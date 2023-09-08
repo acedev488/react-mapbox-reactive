2462fc6e5a03efa52fc8e8d71a505a921e718d1f
Austin Lee <austinl28312@gmail.com>
2023-09-08 10:12:03 -0400

    refactor: trim comment verbosity in MapContext and Marker

--- src/map/MapContext.tsx
+++ src/map/MapContext.tsx
@@ -23,12 +23,7 @@ export interface MapProviderProps {
   children?: ReactNode
 }
 
-/**
- * Boots a single mapboxgl.Map instance and hands it down through context.
- * This is the only place in the tree that touches `new mapboxgl.Map(...)` —
- * every other map-related component (Marker, Layer, ...) is a *consumer*
- * that reacts to this instance rather than owning one itself.
- */
+// Only place that constructs a mapboxgl.Map — everything else consumes it via context.
 export function MapProvider({ accessToken, children }: MapProviderProps) {
   const containerRef = useRef<HTMLDivElement>(null)
   const mapRef = useRef<mapboxgl.Map | null>(null)
@@ -42,11 +37,7 @@ export function MapProvider({ accessToken, children }: MapProviderProps) {
     if (!containerRef.current) return
 
     mapboxgl.accessToken = accessToken
-    // Read once, directly from the store, rather than subscribing via the
-    // `useMapStore` hook — this is only ever used as the map's *initial*
-    // camera (which naturally reflects whatever `persist` already restored
-    // from localStorage before this component even rendered). Reacting to
-    // further store changes here would fight with the moveend sync below.
+    // Read directly (not via the hook) — this is only the *initial* camera.
     const { viewport } = useMapStore.getState()
     const map = new mapboxgl.Map({
       container: containerRef.current,
@@ -57,20 +48,13 @@ export function MapProvider({ accessToken, children }: MapProviderProps) {
       pitch: viewport.pitch,
     })
     mapRef.current = map
-    // The map instance itself is available to consumers immediately —
-    // `isStyleLoaded` is what gates Marker/Layer from touching it too early.
     setValue({ map, isStyleLoaded: false })
 
-    // Fires once for the initial style, and again after every setStyle()
-    // call below — both cases mean "the style is ready, sources/layers can
-    // be (re)added now".
+    // Fires on initial load and again after every setStyle() below.
     const handleStyleReady = () => setValue({ map, isStyleLoaded: true })
     map.on('load', handleStyleReady)
     map.on('style.load', handleStyleReady)
 
-    // One-way sync, map -> store: once the user stops panning/zooming, push
-    // the resulting camera position into zustand so the rest of the app
-    // (a HUD, a "share this view" link, ...) can read it reactively.
     const handleMoveEnd = () => {
       setViewport({
         center: map.getCenter().toArray() as [number, number],
@@ -89,18 +73,13 @@ export function MapProvider({ accessToken, children }: MapProviderProps) {
       mapRef.current = null
       setValue({ map: null, isStyleLoaded: false })
     }
-    // Deliberately excludes `activeBaseStyle` — the *initial* style is set
-    // once here; subsequent changes are handled by the effect below via
-    // `setStyle`, which is a much cheaper operation than tearing the whole
-    // map down and recreating it.
+    // activeBaseStyle intentionally excluded — handled by setStyle() below instead.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessToken, setViewport])
 
-  // Reactive prop (via the store): base style. Swapping styles at runtime
-  // wipes any sources/layers added outside the style JSON, so we flip
-  // `isStyleLoaded` to false first — every mounted <Layer/> tears itself
-  // down in response — then let `style.load` flip it back to true, which
-  // remounts them against the new style.
+  // setStyle() wipes any source/layer added outside the style JSON, so
+  // isStyleLoaded flips false first (every <Layer/> tears down) and back to
+  // true on style.load (they remount against the new style).
   useEffect(() => {
     if (isFirstStyleRef.current) {
       isFirstStyleRef.current = false
@@ -112,12 +91,8 @@ export function MapProvider({ accessToken, children }: MapProviderProps) {
     map.setStyle(BASE_STYLE_URLS[activeBaseStyle])
   }, [activeBaseStyle])
 
-  // Reactive prop (via the store): viewport. This is the store -> map
-  // direction, the counterpart to the moveend handler above. It's guarded
-  // by a distance check rather than a "did I cause this" flag: if the
-  // store's viewport already matches the map's actual camera (which is
-  // exactly what just happened after handleMoveEnd ran), there's nothing to
-  // apply, and skipping avoids fighting the user's own gesture.
+  // store -> map, the other direction from handleMoveEnd. Guarded by a
+  // distance check instead of a flag, so it doesn't fight the user's own gesture.
   useEffect(() => {
     const map = mapRef.current
     if (!map) return
--- src/map/Marker.tsx
+++ src/map/Marker.tsx
@@ -13,17 +13,7 @@ export interface MarkerProps {
   onClick?: () => void
 }
 
-/**
- * Renders nothing. Its entire job is to translate its own React lifecycle
- * (mount / update / unmount) into imperative mapboxgl.Marker calls:
- *
- *   mount    -> new mapboxgl.Marker().addTo(map)
- *   update   -> marker.setLngLat(...)
- *   unmount  -> marker.remove()
- *
- * Parent components never touch mapboxgl directly — they just render/remove
- * <Marker /> elements and pass props, exactly like any other React component.
- */
+// Renders nothing — translates its own mount/update/unmount into mapboxgl.Marker calls.
 export function Marker({
   lngLat,
   color = '#3fb1ce',
@@ -37,25 +27,20 @@ export function Marker({
   const markerRef = useRef<mapboxgl.Marker | null>(null)
   const popupRef = useRef<mapboxgl.Popup | null>(null)
 
-  // Callbacks are read from refs so that changing their identity on every
-  // render doesn't force the mount effect below to re-run.
+  // Read from refs so a new callback identity doesn't retrigger the mount effect.
   const onDragEndRef = useRef(onDragEnd)
   onDragEndRef.current = onDragEnd
   const onClickRef = useRef(onClick)
   onClickRef.current = onClick
 
-  // Mount / unmount: the marker entity's lifetime is tied 1:1 to this
-  // component instance's lifetime, not to any individual prop.
   useEffect(() => {
     if (!map || !isStyleLoaded) return
 
     const marker = new mapboxgl.Marker({ color }).setLngLat(lngLat).addTo(map)
     markerRef.current = marker
 
-    // A marker's DOM element is a sibling of the map canvas, not a child of
-    // it — so a click on it still bubbles up through the map's container
-    // and *also* fires any generic `map.on('click', ...)` listener (e.g.
-    // ClickToAddMarker). stopPropagation() is the documented fix.
+    // Marker elements are siblings of the map canvas, so clicks bubble into
+    // the map's own click handlers (e.g. ClickToAddMarker) unless stopped.
     const handleClick = (e: MouseEvent) => {
       e.stopPropagation()
       onClickRef.current?.()
@@ -71,24 +56,13 @@ export function Marker({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [map, isStyleLoaded])
 
-  // Reactive prop: position. Updated imperatively instead of tearing the
-  // marker down and recreating it.
   useEffect(() => {
     markerRef.current?.setLngLat(lngLat)
   }, [lngLat[0], lngLat[1]])
 
-  // Reactive prop: color. mapboxgl.Marker has no public setColor() — the
-  // constructor option is the only supported way to set it — so an update
-  // here goes around the public API and repaints the marker's own SVG
-  // directly, the same workaround Mapbox's own issue tracker recommends.
-  //
-  // The default marker's SVG (see mapbox-gl-js's `_createDefaultMarker`) is
-  // a flat `<ellipse>` (drop shadow, fill is a gradient url) + `<path>`
-  // (the colored teardrop shape, fill: this._color) + a second `<path>`
-  // (border outline, no fill attribute) + `<circle fill="white">` (the
-  // inner dot). Only the first `<path>`'s fill should track our color prop —
-  // `svg path[fill]` happens to match exactly that one, since the border
-  // path has no `fill` attribute and the circle isn't a `<path>` at all.
+  // mapboxgl.Marker has no public setColor(), so this patches the marker's own
+  // SVG directly. `svg path[fill]` only matches the colored teardrop shape —
+  // the border path has no fill attribute and the inner dot is a <circle>, not a <path>.
   useEffect(() => {
     const marker = markerRef.current
     if (!marker) return
@@ -96,7 +70,6 @@ export function Marker({
     shape?.setAttribute('fill', color)
   }, [color])
 
-  // Reactive prop: draggable — mapboxgl.Marker exposes a real setter for this.
   useEffect(() => {
     const marker = markerRef.current
     if (!marker) return
@@ -113,8 +86,6 @@ export function Marker({
     }
   }, [draggable])
 
-  // Reactive prop: popup text. Created lazily on first use, then just its
-  // text content is swapped on subsequent updates.
   useEffect(() => {
     const marker = markerRef.current
     if (!marker) return
@@ -132,10 +103,8 @@ export function Marker({
     marker.setPopup(popupRef.current)
   }, [popupText])
 
-  // Reactive prop: selected. Mapbox positions markers with an inline
-  // `transform` style, so a CSS class that also sets `transform` would be
-  // silently overridden every time the marker moves. `addClassName` +
-  // a `filter`-based style keeps the highlight independent of positioning.
+  // addClassName (not a plain className prop) since Mapbox already manages
+  // the element's inline transform for positioning.
   useEffect(() => {
     const marker = markerRef.current
     if (!marker) return
